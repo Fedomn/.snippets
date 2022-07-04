d17cfb45b1fa2c49a20f5fe3730bc067f90a5c4d
Fedomn <fedomn.ma@gmail.com>
2022-07-04 12:17:05 +0800

    update mysql

--- mysql/mysql-perf.sql
+++ mysql/mysql-perf.sql
@@ -76,3 +76,10 @@ explain format=json
 select SQL_NO_CACHE count(distinct(weight))
 from test;
 
+-- show table size
+SELECT table_name AS "Table",
+ROUND(((data_length + index_length) / 1024 / 1024), 2) AS "Size (MB)"
+FROM information_schema.TABLES
+WHERE table_schema = "db_name"
+ORDER BY (data_length + index_length) DESC;
+
