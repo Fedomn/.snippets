20433458cbfe96ac70c1fdd8e05de327ad3c7f54
Fedomn <fedomn.ma@gmail.com>
2022-07-14 14:42:27 +0800

    update

--- k8s/kubectl.sh
+++ k8s/kubectl.sh
@@ -1,3 +1,5 @@
 kubectl get pod -o yaml -n namespace my-pod
 kubectl get statefulset -o yaml -n namespace my-pod
+kubectl get replicaset -o yaml -n namespace my-pod
 kubectl logs -n namespace -f my-pod
+kubectl get events --sort-by='.metadata.creationTimestamp' -A
